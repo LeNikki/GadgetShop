0727030e908c3186a95087d08873da62d1734bd7
Nicole Satiembre <80201174+LeNikki@users.noreply.github.com>
2022-10-10 20:47:08 +0800

    oct 10 jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import Home from './Components/Home';
 import './App.css';
+import Home from './Components/Home';
+import Header from './Components/Header';
+import Node from "./Components/Node"
+import Footer from "./Components/Footer"
+import { products } from './Components/Products';
+
 
 function App() {
+
+  const node = products.map((i)=>{
+    return <Node key={i.id} {...i}/>
+  })
+
   return (
     <div className="App">
+      <div className="App">
+      <Header/>
+      <div className='container'>
       <Home/>
+        <div className='node-container'>
+          {node}
+        </div>
+      </div>
+      <Footer/>
+    </div>
     </div>
   );
 }
